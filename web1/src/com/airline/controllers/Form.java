7172fad6955265af04def3ca128066ce1b52a290
Habib Aroua <HabibAroua@users.noreply.github.com>
2017-08-27 22:23:46 +0100

    Validate calendar value

--- web1/src/com/airline/controllers/Form.java
+++ web1/src/com/airline/controllers/Form.java
@@ -1,27 +1,29 @@
 package com.airline.controllers;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.airline.models.Gender;
-
 /**
  * Servlet implementation class Form
  */
+
 @WebServlet("/Form")
-public class Form extends HttpServlet {
+public class Form extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Form() {
+    public Form()
+    {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -50,5 +52,34 @@ public class Form extends HttpServlet {
 		System.out.println("The last name is "+lastName);
 		System.out.println("Date of birth "+dateBirth);
 		System.out.println("Gender is "+gender);
+		
+		if (firstName.length()==0)
+		{
+			System.out.println("Empty first name error");
+			request.setAttribute("error", true);
+		}
+		try
+		{
+			String dobArray[]=dateBirth.split("\\/");
+		
+		    String month=dobArray[0];
+		    String day=dobArray[1];
+		    String year=dobArray[2];
+		
+		    Calendar cal =Calendar.getInstance();
+	        //Type date	
+		    cal.set(Calendar.YEAR , Integer.parseInt(year));
+		    cal.set(Calendar.MONTH , Integer.parseInt(month));
+		    cal.set(Calendar.DAY_OF_MONTH,Integer.parseInt(day));
+		    Date dob=cal.getTime();
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			PrintWriter out=response.getWriter();
+			response.setContentType("text/html");
+			out.println("<script>alert('You don/t enter the birth date !!'); </script>");
+		}
+		
+		
 	}
 }
